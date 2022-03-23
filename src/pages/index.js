4985df06fbf4f7055dc69620ad0fc7d8543f7060
blake-godfrey <97559846+blake-godfrey@users.noreply.github.com>
2022-03-23 11:14:37 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,17 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+// import { Link } from "gatsby"
+// import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-	faCircleXmark
+	faCircleXmark,
+	faCircleCheck
 } from '@fortawesome/free-regular-svg-icons'
 
 import { InlineWidget } from "react-calendly";
-
 
 const IndexPage = () => (
   <Layout>
@@ -21,7 +21,7 @@ const IndexPage = () => (
 		<div className="container mx-auto px-4">
 			<div className="grid grid-cols-1 lg:grid-cols-2 items-center">
 				<div>
-					<h1 className="text-[#dcdcdc]">we help eCommerce & info product businesses <u>generate</u> sales via paid advertising.</h1>
+					<h1 className="text-[#dcdcdc]">We help eCommerce & info product businesses <u>generate</u> sales via paid advertising.</h1>
 					<a href="#book-a-demo" className="btn">Book your free strategy call</a>
 				</div>
 
@@ -39,55 +39,62 @@ const IndexPage = () => (
 		</div>
 	</section>
 
-	<section className="py-12 bg-[#fbfbfd]">
+	<section className="py-12">
 		<div className="container mx-auto px-4">
 			<div className="grid gap-8 grid-cols-1 lg:grid-cols-2">
 				<div>
-					<h2>ads, just ads.</h2>
-					<p>We do one thing - we just do it with a monastic focus and better than anyone else. If you want an agency that offers a full service solution of everything that won’t move the needle forward, we’re not for you.</p>
+					<h2>What we offer</h2>
+					<h3>Paid advertising</h3>
+					<p>If you want an agency that offers a full service solution of everything that won’t move the needle forward, we’re not for you.</p>
 					<p>If you want an agency where with two clicks, you can get a clear breakdown of how much was spent, how much was made & what your net profit was - we’re for you.</p>
-
-					{/* <a href="#" className="btn">Book your free strategy call</a> */}
+					<a href="#book-a-demo" className="btn">Book your free strategy call</a>
 				</div>
 				<div>
-					<h2>we don't offer any other services except for paid advertising...</h2>
+					<h2>We don't offer any other services except for paid advertising...</h2>
 					<ul className="m-0">
-						<li><FontAwesomeIcon icon={faCircleXmark} size="1x" className="text-[#3bb4c1]" /> WEB DESIGN/DEVELOPMENT</li>
-						<li><FontAwesomeIcon icon={faCircleXmark} size="1x" className="text-[#3bb4c1]" /> ​CONTENT CREATION</li>
-						<li><FontAwesomeIcon icon={faCircleXmark} size="1x" className="text-[#3bb4c1]" /> EMAIL MARKETING</li>
-						<li><FontAwesomeIcon icon={faCircleXmark} size="1x" className="text-[#3bb4c1]" /> ​SOCIAL MEDIA MANAGEMENT</li>
-						<li><FontAwesomeIcon icon={faCircleXmark} size="1x" className="text-[#3bb4c1]" /> INSTAGRAM GROWTH</li>
-						<li><FontAwesomeIcon icon={faCircleXmark} size="1x" className="text-[#3bb4c1]" /> PR SERVICE</li>
+						<li><FontAwesomeIcon icon={faCircleXmark} size="1x" className="text-[#3bb4c1] mr-1" /> WEB DESIGN/DEVELOPMENT</li>
+						<li><FontAwesomeIcon icon={faCircleXmark} size="1x" className="text-[#3bb4c1] mr-1" /> ​CONTENT CREATION</li>
+						<li><FontAwesomeIcon icon={faCircleXmark} size="1x" className="text-[#3bb4c1] mr-1" /> EMAIL MARKETING</li>
+						<li><FontAwesomeIcon icon={faCircleXmark} size="1x" className="text-[#3bb4c1] mr-1" /> ​SOCIAL MEDIA MANAGEMENT</li>
+						<li><FontAwesomeIcon icon={faCircleXmark} size="1x" className="text-[#3bb4c1] mr-1" /> INSTAGRAM GROWTH</li>
+						<li><FontAwesomeIcon icon={faCircleXmark} size="1x" className="text-[#3bb4c1] mr-1" /> PR SERVICE</li>
 					</ul>
-				</div>
-				<div>
-					<h2>Lorem ipsum dolor sit amet</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget sagittis ligula. Donec mi dui, auctor ac commodo ut, facilisis ut tortor. Duis egestas arcu a enim cursus egestas. Vestibulum velit ante, rhoncus vel fringilla nec, posuere id tellus. Nullam imperdiet condimentum semper.</p> <p>Phasellus facilisis sem sed libero tempor pretium. Etiam pulvinar cursus massa vel faucibus. Curabitur bibendum viverra placerat. Nullam bibendum quam leo, quis lacinia odio elementum ut. Vivamus ut efficitur enim. Vestibulum tincidunt arcu nunc, a facilisis arcu vulputate sit amet.</p>
-				</div>
-				<div>
-					<h2>book your free strategy call</h2>
-					<InlineWidget url="https://calendly.com/digenerate" />
 				</div>
 			</div>
 		</div>
 	</section>
 
-	{/* <section className="py-12" id="book-a-demo">
+	<section className="py-12 bg-[#fbfbfd]" id="book-a-demo">
 		<div className="container mx-auto px-4">
-			<h2 className="text-center">Book your free strategy call</h2>
-				<div className="max-w-lg mx-auto">
+			<div className="grid gap-8 grid-cols-1 lg:grid-cols-2">
+				<div>
 					<InlineWidget url="https://calendly.com/digenerate" />
 				</div>
-			
+
+				<div>
+					<h2>Book in your free 15 minute demo call</h2>
+					<p>By the end of this Audit call, you will have a clear understanding of the next steps you can take for your business to start generating consistent and reliable results online with Funnels & Paid Advertising.</p>
+					<p>Find a time on Blake’s calendar to schedule your call today and we look forward to speaking to you soon!</p>
+
+					<h3>This audit call is perfect for:</h3>
+					<ul className="m-0">
+						<li><FontAwesomeIcon icon={faCircleCheck} size="1x" className="text-[#3bb4c1] mr-1" /> Businesses looking to convert their current website into a high quality & streamlined funnel format</li>
+						<li><FontAwesomeIcon icon={faCircleCheck} size="1x" className="text-[#3bb4c1] mr-1" /> ​Businesses looking to take their offline business online</li>
+						<li><FontAwesomeIcon icon={faCircleCheck} size="1x" className="text-[#3bb4c1] mr-1" /> Businesses looking to understand their increased revenue potential with funnels & conversion rate optimization</li>
+						<li><FontAwesomeIcon icon={faCircleCheck} size="1x" className="text-[#3bb4c1] mr-1" /> ​Businesses looking to maximize their conversion rates & average order value</li>
+						<li><FontAwesomeIcon icon={faCircleCheck} size="1x" className="text-[#3bb4c1] mr-1" /> Businesses looking for a reliable agency that can make their company a priority</li>
+					</ul>
+				</div>
+			</div>
 		</div>
-	</section> */}
+	</section>
 
 	<section className="py-16 bg-[#2a2a2a]">
 		<div className="container mx-auto px-4">
 			<div className="max-w-lg mx-auto text-center text-[#dcdcdc]">
-				<h2>have a general enquiry?</h2>
-				<p>If you have a general enquiry and would like to speak to our expert team, you can contact us via email at: hello@digenerate.com</p>
-				<a href="#" className="btn">Book your free strategy call</a>
+				<h2>Have a general enquiry?</h2>
+				<p>If you have a general enquiry and would like to speak to our expert team, you can contact us via email at: <a className="text-[#3bb4c1] hover:text-[#2f909a]" href='mailto:hello@digenerate.com'>hello@digenerate.com</a></p>
+				<a href="#book-a-demo" className="btn">Book your free strategy call</a>
 			</div>
 		</div>
 	</section>
